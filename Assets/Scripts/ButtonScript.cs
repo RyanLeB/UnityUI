db14dc17bf2a598f89be5f5b30b46745bfca2202
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-02-26 11:45:29 -0400

    menu made

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonScript : MonoBehaviour
+{
+    public int score = 0;
+    public TextMeshPro scoreCount;
+
+
+    private void Awake()
+    {
+        
+    }
+
+
+    public void ScoreUpdater()
+    {
+
+    }
+
+
+
+}
